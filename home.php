24de79386a5146139cb28595b339a952192b9d19
Namita Maheshwari <mnamita51@gmail.com>
2020-10-10 09:30:59 +0530

    Education Projet

---
+++ home.php
@@ -0,0 +1,24 @@
+<!-- <h1><?php echo $title;?></h1> -->
+<div class="col-lg-12">
+<h2>INTRODUCTION</h2>
+    <p>A virtual classroom is an online learning environment that allows teachers and students to communicate, interact, collaborate, explain ideas.It enables students to access quality teachers anywhere on the planet so long as they both have a reliable internet connection. This can break down most of the common barriers to synchronous learning: cost, distance and timing</p>
+</div>
+<div  class="col-lg-6">
+  <h2>VISION</h2>
+  <p>We believe students will become productive citizens when they are challenged and mentored in leadership, problem solving, and critical thinking skills. Our student centered education includes the following:
+
+•Students experience a rigorous blended online learning environment that prepares them for advanced education options, future employment, and post-secondary education.</p>
+  <h2>MISSION</h2>
+  <p>By providing a rigorous, relevant, cutting-edge, technology based education EDGILE prepares students for careers, and for the global community of life-long learners by engaging one student at a time.</p>
+  <h2>CHARACTERISTICS</h2>
+  <ul>
+    <li><b> Non-Restricting- </b>
+Edgile allows both learners and instructors around the world to participate in live classes to collaborate and interact</li>
+    <li><b>Affordable-</b>
+The low costs of virtual classrooms are considered to be a major advantage. Learners can save money by not having to worry about travel expenses. Participants also save time since all that is needed is an internet connection.</li>
+    <li><b>Flexible Learning-</b>
+Online classes also allow for the ability to record class as it happens, including any presentation audio and visuals. This means that the content is accessible even after being delivered, an added benefit for those who want a quick refresher, or perhaps did not fully understand the first time.
+</li>
+    <li><b>Practical and Proven</b>
+Synchronous learning is a learning environment where everyone takes part in the learning at the same time. A traditional lecture is an example of this type of learning, and has been used for hundreds of years. Online learning enables this same type of experience, but with far more conveniences and tools.</li>
+  </ul>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+require_once("include/initialize.php");
+if (!isset($_SESSION['StudentID'])) {
+  # code...
+  redirect('login.php');
+}
+$content='home.php';
+$view = (isset($_GET['q']) && $_GET['q'] != '') ? $_GET['q'] : '';
+switch ($view) {
+  case 'lesson':
+    $title = "Lesson";
+    $content = 'lesson.php';
+   # code...
+   break;
+  case 'exercises':
+    $title = "Exercises";
+    $content = 'exercises.php';
+   # code...
+   break;
+  case 'download':
+    $title = "Downloads";
+    $content = 'download.php';
+   # code...
+   break;
+  case 'about':
+    $title = "About Us";
+    $content = 'about.php';
+   # code...
+   break;
+  case 'playvideo':
+    $title = "Play Video";
+    $content = 'playvideo.php';
+   # code...
+   break;
+  case 'viewpdf':
+    $title = "Play Video";
+    $content = 'viewpdf.php';
+   # code...
+   break;
+  case 'question':
+    $title = "Question";
+    $content = 'question.php';
+   # code...
+   break;
+  case 'quizresult':
+    $title = "Result";
+    $content = 'quizresult.php';
+   # code...
+   break;
+   case 'Videocall':
+     $title = "Videocall";
+     $content = 'Videocall.html';
+   #code
+   break;
+   case 'Doubts':
+     $title = "Doubts";
+     $content = 'chatroom/index.html';
+   #code
+   break;
+  default :
+    $title = "Home";
+    $content    = 'home.php';
+}
+require_once("theme/templates.php");
+?>
